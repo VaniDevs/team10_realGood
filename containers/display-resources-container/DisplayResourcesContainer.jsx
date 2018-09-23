3cde9c3e961613835909ddc91f0e95757514c661
zjunyuan <zheng.junyuan@move.com>
2018-09-23 10:56:30 -0700

    Add Filter By
    Add Filter By

--- containers/display-resources-container/DisplayResourcesContainer.jsx
+++ containers/display-resources-container/DisplayResourcesContainer.jsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { Spin, List } from "antd";
+import { Spin, List, Button, Select } from "antd";
 import React, { Component, Fragment } from "react";
 import { Resource } from "../../components";
 import res from "../../res";
@@ -29,12 +29,59 @@ export default class CreateResourceContainer extends Component {
           </div>
         )}
         {this.state.resources !== null && (
-          <List
-            itemLayout="vertical"
-            size="large"
-            dataSource={this.state.resources}
-            renderItem={(resource, key) => <Resource key={key} {...resource} />}
-          />
+          <div>
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "space-between"
+              }}
+            >
+              <div>
+                <span>Filter By Resource: </span>
+                <Select
+                  style={{ width: 120 }}
+                  onChange={async value => {
+                    const response = await axios.get(`${res.api}/${value}`);
+                    this.setState({
+                      ...this.state,
+                      resources: response.data
+                    });
+                  }}
+                >
+                  <Select.Option value="healthcare">Healthcare</Select.Option>
+                  <Select.Option value="medical">Medical</Select.Option>
+                  <Select.Option value="food">Food</Select.Option>
+                  <Select.Option value="clothing">Clothing</Select.Option>
+                  <Select.Option value="shelter">Shelter</Select.Option>
+                  <Select.Option value="education">Education</Select.Option>
+                </Select>
+              </div>
+              {localStorage.getItem(res.auth) === "true" && (
+                <Button
+                  onClick={() => {
+                    window.location = "/create";
+                  }}
+                >
+                  Create Resource
+                </Button>
+              )}
+            </div>
+            <div
+              style={{
+                marginTop: "20px"
+              }}
+            >
+              <List
+                itemLayout="vertical"
+                size="large"
+                dataSource={this.state.resources}
+                renderItem={(resource, key) => (
+                  <Resource key={key} {...resource} />
+                )}
+              />
+            </div>
+          </div>
         )}
       </Fragment>
     );
